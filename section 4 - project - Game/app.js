21dcf6f4f174b186dabb5b5f14ce21581d764ca3
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2022-07-18 11:30:48 +0200

    :sparkles: ajout des logs & nouvelle partie

--- section 4 - project - Game/app.js
+++ section 4 - project - Game/app.js
@@ -9,6 +9,7 @@ const app = Vue.createApp({
       monsterHealth: 100,
       currentRound: 0,
       winner: null,
+      logMessages: [],
     };
   },
   watch: {
@@ -20,7 +21,6 @@ const app = Vue.createApp({
       }
     },
     monsterHealth(value) {
-      console.log(this.winner);
       if (value <= 0 && this.playerHealth <= 0) {
         this.winner = 'draw';
       } else if (value <= 0) {
@@ -29,40 +29,62 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+      this.logMessages = [];
+    },
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'attack', attackValue);
+
       this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
       this.playerHealth -= attackValue;
+      this.addLogMessage('monster', 'attack', attackValue);
     },
     specialAttackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(10, 25);
       this.monsterHealth -= attackValue;
+      this.addLogMessage('player', 'Special-Attack', attackValue);
+
       this.attackPlayer();
-      if (this.playerHealth <= 0) {
-        //player lost
-      }
     },
     healPlayer() {
       this.currentRound++;
-
       const healValue = getRandomValue(8, 20);
       if (this.playerHealth + healValue > 100) this.playerHealth = 100;
       else this.playerHealth += healValue;
-
+      this.addLogMessage('player', 'heal', healValue);
       this.attackPlayer();
     },
+    surrender() {
+      this.winner = 'monster';
+      this.logs.push('<h3>You surrended</h3>');
+    },
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
+    },
   },
+
   computed: {
     monsterBarStyle() {
+      if (this.monsterHealth < 0) return { width: '0%' };
       return { width: this.monsterHealth + '%' };
     },
     playerBarStyle() {
+      if (this.playerHealth < 0) return { width: '0%' };
       return { width: this.playerHealth + '%' };
     },
     mayUseSpecialAttack() {
@@ -72,8 +94,3 @@ const app = Vue.createApp({
 });
 
 app.mount('#game');
-
-// #monster
-// #player
-// #controls
-// #log
