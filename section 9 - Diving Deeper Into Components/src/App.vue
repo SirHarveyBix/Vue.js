6d4d27b49f27e7f18c4cec5067e9c9f86cb25018
SirHarvey <gui.lefort.17@gmail.com>
2022-07-21 17:32:45 +0200

    :sparkles: scoping style + local components + :memo:

--- section 9 - Diving Deeper Into Components/src/App.vue
+++ section 9 - Diving Deeper Into Components/src/App.vue
@@ -1,17 +1,27 @@
 <template>
   <div>
-    <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
+    <the-header />
+    <BadgeList />
+    <UserInfo
       :full-name="activeUser.name"
       :info-text="activeUser.description"
       :role="activeUser.role"
-    ></user-info>
+    />
   </div>
 </template>
 
 <script>
+import BadgeList from "./components/BadgeList.vue";
+import TheHeader from "./components/TheHeader.vue";
+import UserInfo from "./components/UserInfo.vue";
+
 export default {
+  // app.component("the-header", TheHeader);
+  components: {
+    "the-header": TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
